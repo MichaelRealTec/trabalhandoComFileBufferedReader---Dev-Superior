a746845bddbad5ca99408a9052c0e42935595f08
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-08-29 23:05:16 -0300

    Resolucao do exercicio com Bloco try-with-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,13 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 		// trabalhandoComFileBufferedReader
 		String path = "c:\\temp\\in.txt"; // O '\' chama comandos especiais
-		FileReader fr = null;
-		BufferedReader br = null;
-
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
 			String line = br.readLine();
 
 			while (line != null) {
@@ -24,18 +19,7 @@ public class Program {
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		} 
 
 	}
 }
